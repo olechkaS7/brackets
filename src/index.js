3ea5febec6b3ba01abeb09791d67018ca95c12b9
olechkaS7 <olg.iurist@yandex.ru>
2020-03-13 16:59:39 +0300

    feat: perfomance brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length;) {
+      if (!str.includes(bracketsConfig[i].join(''))) i+=1;
+      else {
+          str = str.replace(bracketsConfig[i].join(''), '')
+          i = 0;
+      }
+  }
+  if (str.length == 0) return true;
+  else return false;
 }
